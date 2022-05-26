29eec03a54164e64e1392eb291f77789163a6907
cassandra-mn <arthenciac@gmail.com>
2022-05-26 12:10:12 -0300

    categories controller

---
+++ src/controllers/categoriesController.js
@@ -0,0 +1,11 @@
+import connection from '../db.js';
+
+export async function getCategories(req, res) {
+    
+    res.send(200);
+}
+
+export async function postCategories(req, res) {
+
+    res.send(200);
+}
