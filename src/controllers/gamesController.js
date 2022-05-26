e3f499e7f346ca2136f20b8ee0255afc88297697
cassandra-mn <arthenciac@gmail.com>
2022-05-26 14:14:36 -0300

    games controller

---
+++ src/controllers/gamesController.js
@@ -0,0 +1,11 @@
+import connection from '../db.js';
+
+export async function getGames(req, res) {
+
+    res.sendStatus(200);
+}
+
+export async function postGames(req, res) {
+
+    res.sendStatus(201);
+}
