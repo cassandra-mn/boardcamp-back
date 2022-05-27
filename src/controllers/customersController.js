426c0f703e68f3c0eadce5c4ed98e9f8d7ebd2aa
cassandra-mn <arthenciac@gmail.com>
2022-05-27 10:22:28 -0300

    insert customers without rules

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -11,9 +11,11 @@ export async function getCustomers(req, res) {
 }
 
 export async function postCustomers(req, res) {
-    try {
+    const {name, phone, cpf, birthday} = req.body;
 
-        res.sendStatus(501);
+    try {
+        await connection.query('INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4)', [name, phone, cpf, birthday]);
+        res.sendStatus(201);
     } catch(e) {
         console.log(e);
         res.sendStatus(500);
@@ -22,7 +24,7 @@ export async function postCustomers(req, res) {
 
 export async function putCustomers(req, res) {
     try {
-        
+
         res.sendStatus(501);
     } catch(e) {
         console.log(e);
