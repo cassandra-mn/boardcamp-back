ee0b352624aa7b9cfc55edcb3bee0bfaabe42653
cassandra-mn <arthenciac@gmail.com>
2022-05-27 13:57:21 -0300

    customers controller

---
+++ src/controllers/rentalsController.js
@@ -0,0 +1,41 @@
+import connection from '../db.js';
+
+export async function getRentals(res, req) {
+    try {
+
+        res.sendStatus(501);
+    } catch(e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+}
+
+export async function postRentals(res, req) {
+    try {
+        
+        res.sendStatus(501);
+    } catch(e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+}
+
+export async function postRentalsId(res, req) {
+    try {
+        
+        res.sendStatus(501);
+    } catch(e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+}
+
+export async function deleteRentals(res, req) {
+    try {
+        
+        res.sendStatus(501);
+    } catch(e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+}
---
+++ src/routes/rentalsRouter.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+import {getRentals, postRentals, postRentalsId, deleteRentals} from '../controllers/rentalsController.js';
+
+const rentalsRouter = express.Router();
+
+rentalsRouter.get('/rentals', getRentals);
+rentalsRouter.post('/rentals', postRentals);
+rentalsRouter.post('/rentals/:id/return', postRentalsId);
+rentalsRouter.delete('/rentals/:id', deleteRentals);
+
+export default rentalsRouter;
