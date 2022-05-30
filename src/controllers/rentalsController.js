7506f0f5ab02d29b82321d37ce0c6982aba309a7
cassandra-mn <arthenciac@gmail.com>
2022-05-29 23:02:05 -0300

    fix post rentals

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -123,19 +123,29 @@ export async function postRentals(req, res) {
     try {
         if (daysRented <= 0) return res.sendStatus(400);
         
-        const customer = await connection.query(`SELECT * FROM customers WHERE id = ${customerId}`);
+        const customer = await connection.query(`
+            SELECT * FROM customers WHERE id = ${customerId}
+        `);
         if (!customer.rows[0]) return res.sendStatus(400);
         
-        const game = await connection.query(`SELECT * FROM games WHERE id = ${gameId}`);
+        const game = await connection.query(`
+            SELECT * FROM games WHERE id = ${gameId}
+        `);
         if (!game.rows[0]) return res.sendStatus(400);
         
-        const rentals = await connection.query('SELECT * FROM rentals');
+        const rentals = await connection.query(`
+            SELECT * FROM rentals WHERE "gameId" = ${game.rows[0].id}
+        `);
         if (game.rows[0].stockTotal <= rentals.rows.length) return res.sendStatus(400);
         
         const originalPrice = daysRented * game.rows[0].pricePerDay;
         const rentDate = dayjs(Date.now()).format('YYYY-MM-DD');
 
-        await connection.query('INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") VALUES ($1, $2, $3, $4, $5, $6, $7)', [customerId, gameId, rentDate, daysRented, null, originalPrice, null]);        
+        await connection.query(`
+            INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") 
+            VALUES ($1, $2, $3, $4, $5, $6, $7)`, 
+            [customerId, gameId, rentDate, daysRented, null, originalPrice, null]
+        );        
         res.sendStatus(201);
     } catch(e) {
         console.log(e);
